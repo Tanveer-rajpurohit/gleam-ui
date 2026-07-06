fd364dad00353aec09cf4a80d6ae85a6d9d9a3d9
Tanveer <tanveersinghrajpurohit4@gmail.com>
2026-07-06 12:57:28 +0530

    refactor(ui): remove unused Marquee and ComponentGrid components from Page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,10 @@
 
 import { Hero } from "@/components/home/Hero";
-import { Marquee } from "@/components/home/Marquee";
-import { ComponentGrid } from "@/components/home/ComponentGrid";
 
 export default function Page() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
-      <Marquee />
-      <ComponentGrid />
     </div>
   );
 }
